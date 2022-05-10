e8621deae66e109e25e528306addd8b732fd327c
CLg003 <clairemjlaing@gmail.com>
2022-05-10 19:56:30 +0100

    bookStockStartsEmpty test passed.

---
+++ src/main/java/Library.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Library {
+
+    private int capacity;
+    private ArrayList<Book> bookStock;
+
+    public Library(int capacity){
+        this.capacity = capacity;
+        this.bookStock = new ArrayList<>();
+    }
+
+    public int bookCount(){
+        return bookStock.size();
+    }
+}
